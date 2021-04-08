4066a0aa6ae6ca1fbd2777ab29a5c51f441819a5
Norberts Jegers <norberts.jegers@gmail.com>
2021-04-08 22:32:32 +0300

    moved comments to readme file and added simple instruction parsing function

---
+++ README.md
@@ -0,0 +1,459 @@
+# Hack VM Translator
+
+## Arithmetic/Logic commands
+
+| Command | Expression | Return type |
+| ------- | ---------- | ----------- |
+| add     | x+y        | integer     |
+| sub     | x-y        | integer     |
+| neg     | -y         | integer     |
+| eq      | x == 0     | boolean     |
+| gt      | x > y      | boolean     |
+| lt      | x < y      | boolean     |
+| and     | x AND y    | boolean     |
+| or      | x OR y     | boolean     |
+| not     | NOT x      | boolean     |
+
+### Stack pointer position in relation to variables
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;x
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;y
+
+SP -->&nbsp;...
+
+## Segments
+
+-   local
+-   argument
+-   this
+-   that
+-   constant
+-   static
+-   pointer
+-   temp
+
+## Memory map
+
+| Symbol | Address   | Note                              |
+| ------ | --------- | --------------------------------- |
+| SP     | 0         | Stack pointer                     |
+| LCL    | 1         | local segment                     |
+| ARG    | 2         | argument segment                  |
+| THIS   | 3         | this segment                      |
+| THAT   | 4         | that segment                      |
+| temp   | 5..12     |
+| R13    | 13        |
+| R14    | 14        |
+| R15    | 15        |
+| static | 16..255   | these are just Hack ASM variables |
+| stack  | 256..2047 |
+
+## Pointers
+
+Pointer points to THIS or THAT segment base address
+
+```
+THAT = 0
+THIS = 1
+```
+
+## Static
+
+Static vars translate to Hack ASM variables
+
+In file \<filename>.vm static vars translate to Hack ASM vars with name \<filename>.\<i>
+
+## Pseudo code
+
+#### push segment i
+
+```
+addr = segmentPointer + i
+*SP = *addr
+SP++
+```
+
+#### pop segment i
+
+```
+addr = segmentPointer + i
+SP--
+*addr = *SP
+```
+
+#### push constant i
+
+```
+*SP = i
+SP++
+```
+
+#### push pointer 0/1
+
+```
+*SP = THIS/THAT
+SP++
+```
+
+#### pop pointer 0/1
+
+```
+SP--
+THIS/THAT = *SP
+```
+
+## VM code translations to Hack ASM
+
+**NOTE**
+
+\<> are used to denote dynamic values and \{} are references to previously defined code blocks (this is done to reduce code size in this readme file)
+
+addr = segmentPointer + i
+
+```
+@<i>
+D=A
+@<segmentPointer>
+A=D+A
+```
+
+*SP = *addr (presume: A=addr; M=\*addr)
+
+```
+D=M
+@SP
+A=M
+M=D
+```
+
+*addr = *SP (presume: A=&SP; M=SP; D=addr)
+
+```
+@R15
+M=D // save the addr in R15
+@SP
+A=M
+D=M // D = *SP
+@R15
+A=M
+M=D // and finally *addr = *SP
+```
+
+SP++
+
+```
+@SP
+M=M+1
+```
+
+SP--
+
+```
+@SP
+M=M-1
+```
+
+push segment i
+
+```
+{addr = segmentPointer + i}
+{*SP = *addr}
+{SP++}
+```
+
+pop segment i
+
+```
+{addr = segmentPointer + i}
+D=A
+{SP--}
+{*addr = *SP}
+```
+
+push constant i
+
+```
+@<i>
+D=A
+@SP
+A=M
+M=D
+{SP++}
+```
+
+push pointer <0/1>
+
+```
+@<THIS|THAT>
+D=M
+@SP
+A=M
+M=D
+{SP++}
+```
+
+pop pointer <0|1>
+
+```
+{SP--}
+D=M // presuming M = *SP
+@<THIS|THAT>
+M=D
+```
+
+push static i
+
+```
+@<filename>.<i>
+{*SP = *addr}
+{SP++}
+```
+
+pop static i
+
+```
+@<filename>.<i>
+D=A
+{SP--}
+{*addr = *SP}
+
+```
+
+add
+
+```
+
+{SP--}
+A=M
+D=M // this is y
+@R15
+M=D
+{SP--}
+A=M
+D=M // this is x
+@R15
+D=D+M // x+y
+@SP
+M=D
+{SP++}
+
+```
+
+sub
+
+```
+
+{SP--}
+A=M
+D=M
+@R15
+M=D
+{SP--}
+A=M
+D=M
+@R15
+D=D-M
+@SP
+M=D
+{SP++}
+
+```
+
+neg
+
+```
+
+{SP--}
+A=M
+D=-M
+@SP
+M=D
+{SP++}
+
+```
+
+and
+
+```
+
+{SP--}
+A=M
+D=M
+@R15
+M=D
+{SP--}
+A=M
+D=M
+@R15
+D=D&M
+@SP
+M=D
+{SP++}
+
+```
+
+or
+
+```
+
+{SP--}
+A=M
+D=M
+@R15
+M=D
+{SP--}
+A=M
+D=M
+@R15
+D=D|M
+@SP
+M=D
+{SP++}
+
+```
+
+not
+
+```
+
+{SP--}
+A=M
+D=!M
+@SP
+M=D
+{SP+
+
+```
+
+TRUE
+
+```
+
+(TRUE)
+@SP
+M=-1
+{SP++}
+@R13
+0;JMP
+```
+
+FALSE
+
+```
+(FALSE)
+@SP
+M=0
+{SP++}
+@R13
+0;JMP
+
+```
+
+EQ
+
+```
+
+(EQ)
+{SP--}
+A=M
+D=M
+@R15
+M=D // y
+(SP--)
+A=M
+D=M // x
+@R15
+D=D-M
+@TRUE
+D;JEQ
+@FALSE
+0;JMP
+
+```
+
+GT
+
+```
+
+(GT)
+{SP--}
+A=M
+D=M
+@R15
+M=D // y
+(SP--)
+A=M
+D=M // x
+@R15
+D=D-M
+@TRUE
+D;JGT
+@FALSE
+0;JMP
+
+```
+
+LT
+
+```
+
+(LT)
+{SP--}
+A=M
+D=M
+@R15
+M=D // y
+(SP--)
+A=M
+D=M // x
+@R15
+D=D-M
+@TRUE
+D;JLT
+@FALSE
+0;JMP
+
+```
+
+eq
+
+```
+
+@EQ.<N>.END
+D=A
+@R13
+M=D
+@EQ
+0;JMP
+(EQ.<N>.END)
+
+```
+
+gt
+
+```
+
+@GT.<N>.END
+D=A
+@R13
+M=D
+@GT
+0;JMP
+(GT.<N>.END)
+
+```
+
+lt
+
+```
+
+@LT.<N>.END
+D=A
+@R13
+M=D
+@LT
+0;JMP
+(LT.<N>.END)
+
+```
--- hack-vm-translator.js
+++ hack-vm-translator.js
@@ -4,317 +4,13 @@
 const { createReadStream, createWriteStream } = require('fs');
 const { createInterface } = require('readline');
 
-/**
- *
- * Arithmetic/Logic commands
- * add | x+y     | integer
- * sub | x-y     | integer
- * neg | -y      | integer
- * eq  | x == 0  | boolean
- * gt  | x > y   | boolean
- * lt  | x < y   | boolean
- * and | x AND y | boolean
- * or  | x OR y  | boolean
- * not | NOT x   | boolean
- * ------------------------
- *       x
- *       y
- * SP -> ..
- *
- * Segments
- * local
- * argument
- * this
- * that
- * constant
- * static
- * pointer
- * temp
- *
- * Memory map
- * SP     0 Stack pointer
- * LCL    1 local segment
- * ARG    2 argument segment
- * THIS   3 this segment
- * THAT   4 that segment
- * temp   5..12
- * R13    13
- * R14    14
- * R15    15
- * static 16..255 (these are just Hack ASM variables)
- * stack  256..2047
- *
- * pointer is either 0/1 (points to THIS or THAT segment base address)
- * THAT = 0
- * THIS = 1
- *
- * Pointers in general
- * ptr  = <addr>
- * &ptr = Memory[<ptr>]
- * *ptr = Memory[<addr>]
- *
- * push segment i
- * addr = segmentPointer + i; *SP = *addr; SP++
- *
- * pop segment i
- * addr = segmentPointer + i; SP--; *addr = *SP
- *
- * push constant i
- * *SP = i; SP++
- *
- * push pointer 0/1
- * *SP = THIS/THAT; SP++
- *
- * pop pointer 0/1
- * SP--; THIS/THAT = *SP
- *
- * Static
- * Static vars translate to Hack ASM variables
- * In file <filename>.vm static vars translate to Hack ASM vars with name <filename>.<i>
- *
- *
- * Lets try writing Hack assembly here to see how to actually implement
- *
- * // addr = segmentPointer + i
- * @<i>
- * D=A
- * @<segmentPointer(LCL|ARG|THIS|THAT|5(temp))>
- * A=D+A
- *
- * // *SP = *addr (presume: A=addr; M=*addr)
- * D=M
- * @SP
- * A=M
- * M=D
- *
- * // *addr = *SP (presume: A=&SP; M=SP; D=addr)
- * @R15
- * M=D // save the addr in R15
- * @SP
- * A=M
- * D=M // D = *SP
- * @R15
- * A=M
- * M=D // and finally *addr = *SP
- *
- * // SP++
- * @SP
- * M=M+1
- *
- * // SP--
- * @SP
- * M=M-1
- *
- * // push segment i
- * {addr = segmentPointer + i}
- * {*SP = *addr}
- * {SP++}
- *
- * // pop segment i
- * {addr = segmentPointer + i}
- * D=A
- * {SP--}
- * {*addr = *SP}
- *
- * // push constant <CONST>
- * @<CONST>
- * D=A
- * @SP
- * A=M
- * M=D
- * {SP++}
- *
- * // push pointer <0|1>
- * @<THIS|THAT>
- * D=M
- * @SP
- * A=M
- * M=D
- * {SP++}
- *
- * // pop pointer <0|1>
- * {SP--}
- * D=M // presuming M = *SP
- * @<THIS|THAT>
- * M=D
- *
- * // push static i
- * @<filename>.<i>
- * {*SP = *addr}
- * {SP++}
- *
- * // pop static i
- * @<filename>.<i>
- * D=A
- * {SP--}
- * {*addr = *SP}
- *
- * // add
- * {SP--}
- * A=M
- * D=M // this is y
- * @R15
- * M=D
- * {SP--}
- * A=M
- * D=M // this is x
- * @R15
- * D=D+M // x+y
- * @SP
- * M=D
- * {SP++}
- *
- * // sub
- * {SP--}
- * A=M
- * D=M
- * @R15
- * M=D
- * {SP--}
- * A=M
- * D=M
- * @R15
- * D=D-M
- * @SP
- * M=D
- * {SP++}
- *
- * // neg
- * {SP--}
- * A=M
- * D=-M
- * @SP
- * M=D
- * {SP++}
- *
- *
- * (TRUE)
- *     @SP
- *     M=-1
- *     {SP++}
- *     @R13
- *     0;JMP
- * (FALSE)
- *     @SP
- *     M=0
- *     {SP++}
- *     @R13
- *     0;JMP
- *
- * (EQ)
- * {SP--}
- * A=M
- * D=M
- * @R15
- * M=D // y
- * (SP--)
- * A=M
- * D=M // x
- * @R15
- * D=D-M
- * @TRUE
- * D;JEQ
- * @FALSE
- * 0;JMP
- *
- * (GT)
- * {SP--}
- * A=M
- * D=M
- * @R15
- * M=D // y
- * (SP--)
- * A=M
- * D=M // x
- * @R15
- * D=D-M
- * @TRUE
- * D;JGT
- * @FALSE
- * 0;JMP
- *
- * (LT)
- * {SP--}
- * A=M
- * D=M
- * @R15
- * M=D // y
- * (SP--)
- * A=M
- * D=M // x
- * @R15
- * D=D-M
- * @TRUE
- * D;JLT
- * @FALSE
- * 0;JMP
- *
- * // eq
- * @EQ.<N>.END
- * D=A
- * @R13
- * M=D
- * @EQ
- * 0;JMP
- * (EQ.<N>.END)
- *
- * // gt
- * @GT.<N>.END
- * D=A
- * @R13
- * M=D
- * @GT
- * 0;JMP
- * (GT.<N>.END)
- *
- * // lt
- * @LT.<N>.END
- * D=A
- * @R13
- * M=D
- * @LT
- * 0;JMP
- * (LT.<N>.END)
- *
- * // and
- * {SP--}
- * A=M
- * D=M
- * @R15
- * M=D
- * {SP--}
- * A=M
- * D=M
- * @R15
- * D=D&M
- * @SP
- * M=D
- * {SP++}
- *
- * // or
- * {SP--}
- * A=M
- * D=M
- * @R15
- * M=D
- * {SP--}
- * A=M
- * D=M
- * @R15
- * D=D|M
- * @SP
- * M=D
- * {SP++}
- *
- * // not
- * {SP--}
- * A=M
- * D=!M
- * @SP
- * M=D
- * {SP++}
- *
- */
+ const SegmentCodes = {
+    local: 'LCL',
+    argument: 'ARG',
+    this: 'this',
+    that: 'that',
+    temp: 5
+};
 
 const CodeBlocks = {
     setAddress: (segment, i) => [
@@ -406,37 +102,37 @@ const CodeBlocks = {
         'M=D',
         ...CodeBlocks.incrementStackPointer()
     ],
-    add: () => [
+    add: [
         ...CodeBlocks.setXY(),
         '@R15',
         'D=D+M',
         ...CodeBlocks.pushResultToStack()
     ],
-    sub: () => [
+    sub: [
         ...CodeBlocks.setXY(),
         '@R15',
         'D=D-M',
         ...CodeBlocks.pushResultToStack()
     ],
-    neg: () => [
+    neg: [
         ...CodeBlocks.decrementStackPointer(),
         'A=M',
         'D=-M',
         ...CodeBlocks.pushResultToStack()
     ],
-    and: () => [
+    and: [
         ...CodeBlocks.setXY(),
         '@R15',
         'D=D&M',
         ...CodeBlocks.pushResultToStack()
     ],
-    or: () => [
+    or: [
         ...CodeBlocks.setXY(),
         '@R15',
         'D=D|M',
         ...CodeBlocks.pushResultToStack()
     ],
-    not: () => [
+    not: [
         ...CodeBlocks.decrementStackPointer(),
         'A=M',
         'D=!M',
@@ -495,6 +191,87 @@ const CodeBlocks = {
         '0;JMP',
         `(GT.${n}.END)`
     ]
+};
+
+const Commands = [
+    'add',
+    'sub',
+    'neg',
+    'and',
+    'or',
+    'not',
+    'eq',
+    'lt',
+    'gt'
+];
+
+const ComparisonCommands = [
+    'eq',
+    'lt',
+    'gt'
+];
+
+// indicates if we need to add comparison code blocks at the top of the ASM file
+let comparisonCommandsUsed = false;
+let eqCount = 0;
+let ltCount = 0;
+let gtCount = 0;
+
+function parseInstruction(instruction, fileName) {
+    if (Commands.includes(instruction)) {
+        if (ComparisonCommands.includes(instruction)) {
+            comparisonCommandsUsed = true;
+            let block;
+
+            switch (instruction) {
+                case 'eq':
+                    block = CodeBlocks.eq(eqCount);
+                    eqCount++;
+                    break;
+                case 'lt':
+                    block = CodeBlocks.lt(ltCount);
+                    ltCount++;
+                    break;
+                case 'gt':
+                    block = CodeBlocks.gt(gtCount);
+                    gtCount++;
+                    break;
+            }
+
+            return block;
+        } else {
+            return CodeBlocks[instruction];
+        }
+    } else {
+        const [operation, segment, i] = instruction.split(' ');
+
+        if (Object.keys(SegmentCodes).includes(segment)) {
+            if (operation === 'push') {
+                return CodeBlocks.pushSegment(SegmentCodes[segment], i);
+            }
+
+            return CodeBlocks.popSegment(SegmentCodes[segment], i);
+        } else {
+            switch (segment) {
+                case 'constant':
+                    return CodeBlocks.pushConstant(i);
+                case 'static':
+                    if (operation === 'push') {
+                        return CodeBlocks.pushStatic(fileName, i);
+                    }
+
+                    return CodeBlocks.popStatic(fileName, i);
+                case 'pointer':
+                    if (operation === 'push') {
+                        return CodeBlocks.pushPointer(i);
+                    }
+                    
+                    return CodeBlocks.popPointer(i);
+                default:
+                    throw new Error('Invalid memory segment');
+            }
+        }
+    }
 }
 
 function translate(input) {
